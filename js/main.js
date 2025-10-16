cd2c36208534d409be0f09ab7fb034102d7f8dd9
Valentin Sierra <valentinsierradw@gmail.com>
2025-10-16 12:39:21 -0300

    16/10 12:39AM

--- js/main.js
+++ js/main.js
@@ -3,3 +3,5 @@ import Game from './game.js';
 const boardContainer = document.getElementById('board');
 const game = new Game(boardContainer);
 console.log('Game initialized:', game);
+
+/*game.clear() limpia el tablero*/
