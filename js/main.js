fcdf395ef76bcf02b6707b75bc82999de8eabe3f
Valentin Sierra <valentinsierradw@gmail.com>
2025-11-05 15:08:00 -0300

    visuales

--- js/main.js
+++ js/main.js
@@ -9,4 +9,8 @@ const currentPosition = game.board.boardArray();
 
 ai.minimax(currentPosition, 1, true); //le paso la currentPosition, depth y true = maxPlayer! (cambiar depth si queres que piense mas o menos)
 
-/*game.clear() limpia el tablero*/
+document.getElementById('new-game').addEventListener('click', () => {
+    game.clear();
+    let newGame = new Game(boardContainer);
+    console.log('[Game]New game started:', newGame);
+});
